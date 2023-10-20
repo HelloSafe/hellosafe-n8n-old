4dff55c9a19c6511caa2aa75297b023353cc53e6
supermario12342003 <supermario12342003@gmail.com>
2023-10-20 11:50:00 +0200

    update n8n use postgres now

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@
 # check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT="$PORT"; echo "N8N will start on '$PORT'"; fi
 
-export DB_TYPE=$N8N_DB_TYPE
-export DB_MYSQLDB_DATABASE=$N8N_DB_DATABASE
-export DB_MYSQLDB_HOST=$N8N_DB_HOST
-export DB_MYSQLDB_PORT=$N8N_DB_PORT
-export DB_MYSQLDB_USER=$N8N_DB_USER
-export DB_MYSQLDB_PASSWORD=$N8N_DB_PASSWORD
+echo "database host: $N8N_DB_HOST post: $N8N_DB_PORT database: $N8N_DB_DATABASE"
+
+export DB_TYPE=postgresdb
+export DB_POSTGRESDB_HOST=$N8N_DB_HOST
+export DB_POSTGRESDB_PORT=$N8N_DB_PORT
+export DB_POSTGRESDB_DATABASE=$N8N_DB_DATABASE
+export DB_POSTGRESDB_USER=$N8N_DB_USER
+export DB_POSTGRESDB_PASSWORD=$N8N_DB_PASSWORD
 
 # Activates automatic data pruning
 export EXECUTIONS_DATA_PRUNE=true
